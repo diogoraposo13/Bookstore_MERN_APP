9fdbb01a8f5927c0071b1a2aeb7a6940a4bee83c
diogoraposo13 <diogo.raposo@live.com.pt>
2023-09-02 02:50:21 +0100

    Get all books and get single book by Id

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from 'mongoose';
 import {Book} from './models/bookModel.js'
@@ -38,6 +38,40 @@ app.post('/books', async (request, response) =>  {
         response.status(500).send({message: error.message})
     }
 });
+
+app.get('/books', async (request,response) =>{
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count:books.length,
+            data:books
+        });
+    
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+
+app.get('/books/:id', async (request,response) =>{
+    try {
+
+const {id} = request.params;
+
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    
+        
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+})
+
+
+
 
 mongoose
 .connect(mongoDBURL)
