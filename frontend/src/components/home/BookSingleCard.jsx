a90bcc531a9bb4ff72cc1825f3c3cf8b3879b1db
diogoraposo13 <diogo.raposo@live.com.pt>
2023-09-03 19:32:10 +0100

    Add Book Modal

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -5,8 +5,12 @@ import { BiUserCircle } from "react-icons/bi";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
+import { BiShow } from "react-icons/bi";
+import { useState } from "react";
+import BookModal from './BookModal';
 
 const BookSingleCard = ({book}) => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <div
       key={book._id}
@@ -27,6 +31,12 @@ const BookSingleCard = ({book}) => {
       </div>
 
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
+        <BiShow 
+          className='text-3xl text-blue-800 hover:text-black cursor-pointer'
+          onClick={() => setShowModal(true)}
+        />
+
+
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
         </Link>
@@ -39,6 +49,12 @@ const BookSingleCard = ({book}) => {
           <MdOutlineDelete className="text-2xl text-red-600" />
         </Link>
       </div>
+      {
+        showModal && (
+          <BookModal book={book} onClose={() => setShowModal(false)}/>
+        )
+      }
+
     </div>
   );
 };
